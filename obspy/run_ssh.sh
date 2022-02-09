5b717b3590ed7b7d06cd237515d4e006c52d89f1
Zhuo-Kang Guan <z.k.guan@g.ncu.edu.tw>
2022-02-09 17:29:55 +0800

    CRLF to LF

--- obspy/run_ssh.sh
+++ obspy/run_ssh.sh
@@ -1,9 +1,9 @@
 docker container rm -f obspy_ssh
 docker run -d \
-        -p 49154:22 \
-        -p 8888:8888 \
+        -p 3333:22 \
         -v /etc/passwd:/etc/passwd:ro \
         -v /etc/shadow:/etc/shadow:ro \
         -v /etc/group:/etc/group:ro \
-        -v <path/to/workspace>:/home/${USER} \
-        --name obspy_ssh sgylab/obspy
+        -v /mnt/e/docker/NP2021:/home/${USER} \
+        -v /mnt/e/pycharm/Data_NP2021:/home/${USER}/data:ro \
+        --name obspy_ssh zkguan/obspy
